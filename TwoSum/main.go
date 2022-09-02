cd5d25ef695eecf86de130cac8e1f42935e8311d
Christian <3003html@gmail.com>
2022-09-02 14:37:15 -0500

    algorith created v1

--- TwoSum/main.go
+++ TwoSum/main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
+
+func TwoSum(nums []int, target int) []int {
+	c := 1
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > int(math.Pow10(9)) || nums[i] < int(math.Pow(-10, 9)) {
+			return []int{}
+		}
+		cont := c
+		for cont < len(nums) {
+
+			val := nums[i] + nums[cont]
+			if val == target {
+				return []int{i, cont}
+			}
+			cont++
+		}
+		c++
+	}
+	return []int{}
+}
 
 func main() {
 	arrT := []int{3, 2, 3}
 	tar := 6
-	out := TwoSumConc2(arrT, tar)
+	out := TwoSum(arrT, tar)
 	fmt.Println(out)
 }
