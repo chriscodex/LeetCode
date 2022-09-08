88f81a3615cece0b33c7cec2c63db29dfa1be3c2
Christian <3003html@gmail.com>
2022-09-08 03:12:43 -0500

    algorithm isValid done

--- validParentheses/main.go
+++ validParentheses/main.go
@@ -1,5 +1,25 @@
 package main
 
 func isValid(s string) bool {
-	return false
+	if len(s)%2 != 0 {
+		return false
+	}
+	stack := []rune{}
+	m := map[rune]rune{
+		'(': ')',
+		'{': '}',
+		'[': ']',
+	}
+	for _, e := range s {
+		if value, ok := m[e]; ok {
+			stack = append(stack, value)
+			continue
+		}
+		l := len(stack) - 1
+		if l < 0 || stack[l] != e {
+			return false
+		}
+		stack = stack[:l]
+	}
+	return len(stack) == 0
 }
