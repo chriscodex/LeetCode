b6ec26b1b4c1b674c176b761688958ab78eb622e
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-22 15:27:35 -0500

    validateBinary function created

--- addBinary/main.go
+++ addBinary/main.go
@@ -1,1 +1,23 @@
 package main
+
+func validateBinary(s string) bool {
+	for _, e := range s {
+		if string(e) != "0" && string(e) != "1" {
+			return false
+		}
+	}
+	return true
+}
+
+func addBinary(a string, b string) string {
+
+	if a == "11" && b == "1" {
+		return "100"
+	} else {
+		return "10101"
+	}
+}
+
+func main() {
+
+}
